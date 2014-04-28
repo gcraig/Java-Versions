ad4db0158d38fea92a31c5b8ebe7a4934d1a0bb7
gcraig <georgeacraig@gmail.com>
2014-04-28 03:45:42 -0500

    added diamond operator

--- 1.7/NewFeatures.java
+++ 1.7/NewFeatures.java
@@ -1,4 +1,6 @@
 import java.io.IOException;
+import java.util.List;
+import java.util.ArrayList;
 
 // Java 7
 public class NewFeatures {
@@ -6,7 +8,14 @@ public class NewFeatures {
     // underscore commana
     static int millions = 1_000_000;
     
+    // diamond operator
+    static List<String> list = new ArrayList<>();
+
     public static void main(String[] args) {
+        
+        list.add("Michael");
+        list.add("Jordan");
+        System.out.println(list);
         
         System.out.println(millions);
         
