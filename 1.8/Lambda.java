803207f908a87c025e74a365553053f351205b6e
gcraig <georgeacraig@gmail.com>
2014-04-28 23:14:54 -0500

    added lambda, colon operator

--- 1.8/Lambda.java
+++ 1.8/Lambda.java
@@ -53,11 +53,19 @@ public class Lambda {
 	}
 
 	void printLambdaList() {
-		//Old way:
+		
 		List<Integer> list = Arrays.asList(1, 2, 3, 4, 5, 6, 7);
+
+		//Old way:
 		for(Integer n: list) {
     		System.out.println(n);
 		}
+
+		//New way:
+		list.forEach(n -> System.out.println(n));
+
+		//Double colon operator
+		list.forEach(System.out::println);
 	}
 	
 	public static void main(String[] args) {
