69f5961c9689bc3298030cbffcea57e2a473ba7f
gcraig <georgeacraig@gmail.com>
2014-04-28 23:10:31 -0500

    added lambda example

---
+++ 1.8/Lambda.java
@@ -0,0 +1,72 @@
+
+import java.util.List;
+import java.util.Arrays;
+
+public class Lambda {
+
+	void runThreads() {
+
+		Runnable run = new Runnable() {
+		    public void run() {
+		        try {
+		            for (int i = 0; i < 5; i++) {
+		                Thread.sleep(1000);
+		                System.out.print(i + "\n" + "..");
+		            }
+
+		        } catch (InterruptedException e) {
+		            System.out.println(" interrupted");
+		        }
+		    }
+ 		};
+ 		
+ 		new Thread(run).start();
+ 		new Thread(run).start();
+
+	}
+
+	void runLambdaThreads() {
+
+		Runnable run = () -> {
+	        try {
+	            for (int i = 0; i < 5; i++) {
+	                Thread.sleep(1000);
+	                System.out.print(i + "\n" + "..");
+	            }
+
+	        } catch (InterruptedException e) {
+	            System.out.println(" interrupted");
+	        }
+		};
+
+ 		new Thread(run).start();
+ 		new Thread(run).start();
+
+	}
+
+	void runLambdaThreads2() {
+
+		new Thread(
+				() -> System.out.println("hello world")
+			)
+			.start();
+	}
+
+	void printLambdaList() {
+		//Old way:
+		List<Integer> list = Arrays.asList(1, 2, 3, 4, 5, 6, 7);
+		for(Integer n: list) {
+    		System.out.println(n);
+		}
+	}
+	
+	public static void main(String[] args) {
+
+		Lambda l = new Lambda();
+		// l.runThreads();
+		l.runLambdaThreads();
+		l.runLambdaThreads2();
+		l.printLambdaList();
+
+	}
+}
